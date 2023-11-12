d2b83c360b4f417ea4cff2cea356bf701eeb5033
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-11-12 20:44:21 +0700

    add table data

--- src/dashboard/products/products.tsx
+++ src/dashboard/products/products.tsx
@@ -1,10 +1,11 @@
-import { Autocomplete, Box, Button, Grid, Stack, TextField, Typography } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import MUIDataTable from "mui-datatables";
 import {useEffect, useState} from 'react'
 import Sidenav from "../../navigation/sidebar";
+import AddCircleIcon from "@mui/icons-material/AddCircle"
+
 import DeleteIcon from "@mui/icons-material/Delete"
 import EditIcon from "@mui/icons-material/Edit"
-import AddCircleIcon from "@mui/icons-material/AddCircle"
 
 export default function Product() {
     const [products, setProduct] = useState([] as any)
@@ -20,55 +21,77 @@ export default function Product() {
     }, []);
     
     const columns = [
-    { 
-        field: "id", 
-        headerName: "ID" 
-    },
-    {
-        field: "name",
-        headerName: "Name",
-        cellClassName: "name-column--cell",
-        flex: 1
-    },
-    {
-        field: "phone",
-        headerName: "Phone Number",
-        flex: 1
-    },
-    {
-        field: "email",
-        headerName: "Email",
-        flex: 1
-    },
-    {
-        field: "cost",
-        headerName: "Cost",
-        flex: 1,
-        renderCell: (params : any) => (
-            <Typography>
-            ${params.row.cost}
-            </Typography>
-        ),
-    },
-    {
-        field: "date",
-        headerName: "Date",
-        flex: 1
-    },
-    {
-        field: "action",
-        headerName: "Action",
-        flex: 1,
-        renderCell: () => (
-            <Stack spacing={2} direction={"row"}>
-                <EditIcon style={{ fontSize: "20px", color: "blue", cursor: "pointer" }}
-                onClick={() => {}}/>
-                <DeleteIcon style={{ fontSize: "20px", color: "darkred", cursor: "pointer" }}
-                onClick={() => {}}/>
-            </Stack>
-        ),
-    },
-];
+        { 
+            name: "id", 
+            label: "ID",
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: "name",
+            label: "Name",
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: "phone",
+            label: "Phone Number",
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: "email",
+            label: "Email",
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: "cost",
+            label: "Cost",
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: "date",
+            label: "Date",
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: "action",
+            label: "Action",
+            options: {
+                filter: true,
+                sort: true,
+                customBodyRender: () => (
+                    <Stack spacing={2} direction={"row"}>
+                        <EditIcon style={{ fontSize: "20px", color: "blue", cursor: "pointer" }}
+                        onClick={() => {}}/>
+                        <DeleteIcon style={{ fontSize: "20px", color: "darkred", cursor: "pointer" }}
+                        onClick={() => {}}/>
+                    </Stack>
+                ),
+            },
+        },
+    ];
+
+    const options = {
+        onRowsDelete: (rowsDeleted: any, newTableData : any[]) => {
+            console.log(`${rowsDeleted}${newTableData.filter(item => products.includes(item))}`)
+        },
+    };
 
     return (
         <Box height={70}>
@@ -87,20 +110,11 @@ export default function Product() {
                             display={'contents'}
                         >
                             <Box height={10}/>
-                            <Stack spacing={2} direction={"row"} className="my-2 mb-2">
-                                <Autocomplete disablePortal
-                                id="combo-search"
-                                options={products}
-                                sx={{ width: 300 }}
-                                getOptionLabel={(products : any) => products.name}
-                                renderInput={(params) => (<TextField {...params} size="small" label="Search"/>)}/>
-                                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
-                                <Button variant="contained" endIcon={<AddCircleIcon/>}>
-                                    Add
-                                </Button>
-                            </Stack>
+                            <Button variant="contained" endIcon={<AddCircleIcon/>}>
+                                Add
+                            </Button>
                             <Box height={10}/>
-                            <DataGrid rows={products} columns={columns} sx={{ margin: "auto" }}/>
+                            <MUIDataTable title={""} data={products} columns={columns} options={options}/>
                         </Box>
                     </Box>
                 </Box>
